40b78119ebbe7795e8f31dd80ffafd42964f346c
Uzo Olisemeka <uzo@designbymobius.ca>
2018-04-20 13:30:52 -0400

    app runs in cron mode if duration is specified, or as a one-time script if no cron duration is given

--- app.js
+++ app.js
@@ -1,59 +1,15 @@
-// require .env packacge
-require("dotenv").config();
+// load env variables
+  require('dotenv').config();
 
-// require twitter package
-var TwitterPackage = require("twitter");
+// get cron duration
+  var cron_duration_hrs = parseInt( process.env.TWEETER_CRON_DURATION_HRS );
 
-// require underscore-node
-var _ = require('underscore-node');
+if( cron_duration_hrs ){
+  console.log( 'cron duration detected. running in cron mode' );
+  require('./components/cron');
+}
 
-// twitter api keys
-var secret = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-};
-
-// make a new twitter object that takes in the api keys
-var Twitter = new TwitterPackage(secret);
-
-// get reques to yoruba names api
-function retrieveTweet () {
-  const http = require("http");
-  const url =
-    "http://www.yorubaname.com/v1/names?count=5000";
-  http.get(url, res => {
-    res.setEncoding("utf8");
-    let body = "";
-    res.on("data", data => {
-      body += data;
-    });
-    res.on("end", () => {
-      body = JSON.parse(body);
-
-      // pick a random name from JSON repsonse without repeating
-      var randomName = _.shuffle(body)[0];
-      console.log(randomName);
-      // create link for random name
-      var linkBack = "http://www.yorubaname.com/entries/" + randomName["name"];
-      console.log(randomName["name"]+ "\n" + randomName["meaning"] + "\n" + linkBack);
-
-
-      //posting to twitter
-      Twitter.post("statuses/update", {status:randomName["name"]+ "\n" + randomName["meaning"] + "\n" + linkBack},  function(error, tweet, response){
-          if(error){
-             console.log(error);
-            }
-         console.log(tweet);  // Tweet body.
-         console.log(response);  // Raw response object.
-       });
-
-    });
-  })
-};
-
-const init = () => retrieveTweet();
-
-// Expose the `init` function, so it can be administered by the clock/cron module.
-module.exports = init;
+else {
+  console.log( 'cron duration not detected. running as one-time script' );
+  require('./components/tweeter')();
+}
